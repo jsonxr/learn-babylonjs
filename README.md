f139c08314528c3b21b81fab001633bb4ebd3adb
Jason Rowland <jason@snowcapstudios.com>
2019-05-09 11:06:42 -0500

    Added Textured blocks

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ This is a basic file structure for a babylonjs typescript project. It uses parse
 
 Create a skeleton babylonjs project using typescript and parseljs.
 
+# 2. Textures
+
+We are going to demonstrate using textures by displaying minecraft blocks. In order to complete this demo, you will need to extract the assets from the minecraft jar file.
+
+## Resources
+* https://doc.babylonjs.com/how_to/createbox_per_face_textures_and_colors
+* https://doc.babylonjs.com/resources/multi-platform_compressed_textures
--- src/game.ts
+++ src/game.ts
@@ -2,21 +2,46 @@ import {
   Engine,
   FreeCamera,
   HemisphericLight,
+  Mesh,
   MeshBuilder,
   Scene,
+  StandardMaterial,
+  Texture,
   Vector3,
 } from 'babylonjs';
+
+// @ts-ignore
+import AssetTextureBedrock from '../assets/bedrock.png';
+// @ts-ignore
+import AssetTextureDirt from '../assets/dirt.png';
+// @ts-ignore
+import AssetTextureStone from '../assets/stone.png';
 
 export default class Game {
   private canvas: HTMLCanvasElement;
   private engine: Engine;
   private scene: Scene;
   private camera!: FreeCamera;
+  // Textures
+  private dirt: StandardMaterial;
+  private stone: StandardMaterial;
+  private bedrock: StandardMaterial;
 
   constructor(canvas: HTMLCanvasElement) {
     this.canvas = canvas;
     this.engine = new Engine(this.canvas, true);
     this.scene = new Scene(this.engine);
+    this.scene.useRightHandedSystem = true; // Most graphics software use right handed system
+    // Textures
+    this.dirt = new StandardMaterial('material', this.scene);
+    this.dirt.diffuseTexture = new Texture(AssetTextureDirt, this.scene, undefined, undefined,
+      Texture.NEAREST_SAMPLINGMODE);
+    this.stone = new StandardMaterial('material', this.scene);
+    this.stone.diffuseTexture = new Texture(AssetTextureStone, this.scene, undefined, undefined,
+      Texture.NEAREST_SAMPLINGMODE);
+    this.bedrock = new StandardMaterial('material', this.scene);
+    this.bedrock.diffuseTexture = new Texture(AssetTextureBedrock, this.scene, undefined, undefined,
+      Texture.NEAREST_SAMPLINGMODE);
   }
 
   public createScene(): void {
@@ -35,12 +60,22 @@ export default class Game {
     this.camera.setTarget(Vector3.Zero());
     this.camera.attachControl(this.canvas, false);
 
-    // Sphere
-    const sphere = MeshBuilder.CreateSphere('sphere', {
-      diameter: 1,
-      segments: 16,
-    }, this.scene);
-    sphere.position.y = 1;
+    // Blocks
+    this.setBlock(this.scene, this.bedrock, new Vector3(0, 1, 0));
+    this.setBlock(this.scene, this.stone, new Vector3(1, 1, 0));
+    this.setBlock(this.scene, this.dirt, new Vector3(2, 1, 0));
+  }
+
+  public setBlock(scene: Scene, material: StandardMaterial, position: Vector3): Mesh {
+    const block: Mesh = MeshBuilder.CreateBox('block1', {
+      size: 1,
+    }, scene);
+    block.material = material;
+    position.x = position.x + 0.5;
+    position.z = position.z + 0.5;
+    position.y = position.y - 0.5;
+    block.setAbsolutePosition(position);
+    return block;
   }
 
   public start(): void {
